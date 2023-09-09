1d5ac2cf390d48af8b80492267a1c873e3adf62a
Leonardo Jiménez Ubaque <91491936+LeonardoJimenezUbaque@users.noreply.github.com>
2023-09-09 00:39:59 -0500

    Cambios pruebas
    Se realizaron diversos cambios dejando el script funcional, pero, no funciona el update y delete

--- php-base-de-datos/server.php
+++ php-base-de-datos/server.php
@@ -5,39 +5,59 @@ include 'conexionBD.php';
 
 // Datos del formulario usuarios
 $id = $_POST['id'];
+$nuevoId = $_POST['nuevoId'];
 $nombre = $_POST['nombre'];
 $apellido = $_POST['apellido'];
 $edad = $_POST['edad'];
 $correo = $_POST['correo'];
 $password = $_POST['password'];
 
-// Datos del formulario juegos
-$idjuegos = $_POST['idjuegos'];
-$nombrejuegos = $_POST['nombrejuegos'];
-$categoria = $_POST['categoria'];
-$plataforma = $_POST['plataforma'];
+// Recibe el metodo que se quiere usar en la base de datos
+$opciones = $_POST['opciones'];
 
-// Insertar datos usuarios
-$sqlusuarios = "INSERT INTO registrousuarios (id, nombre, apellido, edad, correo, password)
-VALUES ('$id', '$nombre', '$apellido', '$edad', '$correo', '$password')";
+// Menu de opciones de manejo de datos en la base de datos
+switch ($opciones) {
+    case 'insert': {
+        // Insertar datos usuarios
+        $sqlusuarios = "INSERT INTO registro_usuarios (id, nombre, apellido, edad, correo, password)
+        VALUES ('$id', '$nombre', '$apellido', '$edad', '$correo', '$password')";
+        break;
+    }
+    case 'select': {
+        // Seleccionar datos usuarios
+        $sqlusuarios = "SELECT * FROM registro_usuarios WHERE id = '$id'";
+        $resultado = mysqli_query($conn, $sqlusuarios);
 
-// // Insertar datos juegos
-// $sqljuegos = "INSERT INTO juegos (idjuegos, nombrejuegos, categoria, platforma)
-// VALUES ('$idjuegos', '$nombrejuegos', '$categoria', '$plataforma')";
+        while ($fila = mysqli_fetch_assoc($resultado)) {
+            echo "Nombre: " . $fila["nombre"] . "<br>";
+            echo "Apellido: " . $fila["apellido"] . "<br>";
+            echo "Edad: " . $fila["edad"] . "<br>";
+            echo "Correo: " . $fila["correo"] . "<br>";
+            echo "Password: " . $fila["password"] . "<br>";
+        }
+        break;
+    }
+    case 'update': {
+        // Actualizar datos usuarios
+        $sqlusuarios = "UPDATE registro_usuarios
+        SET nombre = '$nombre', apellido = '$apellido', edad = '$edad', correo = '$correo', password = '$password'
+        WHERE id = '$id'";
+        break;
+    }
+    case 'delete': {
+        // Eliminar datos usuarios
+        $sqlusuarios = "DELETE FROM registro_usuarios
+        WHERE id = '$id'";
+        break;
+    }
+}
 
 // Chequear conexion usuarios
 if (mysqli_query($conn, $sqlusuarios)) {
-    echo "New record created successfully";
+    echo "Todo correcto";
 } else {
     echo "Error: " . $sqlusuarios . "<br>" . mysqli_error($conn);
 }
-
-// // Chequear conexion juegos
-// if (mysqli_query($conn, $sqljuegos)) {
-//     echo "New record created successfully";
-// } else {
-//     echo "Error: " . $sqljuegos . "<br>" . mysqli_error($conn);
-// }
 
 // Cerrar conexion
 $conn->close();
